ee188c59ec350811121eee51419760ced58d88eb
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-06-07 15:35:40 +0100

    Rename Avatar package

--- app/src/main/java/com/layer/messenger/MessagesListActivity.java
+++ app/src/main/java/com/layer/messenger/MessagesListActivity.java
@@ -159,7 +159,7 @@ public class MessagesListActivity extends BaseActivity {
                 .setHistoricMessagesPerFetch(20);
 
         mMessagesList = ((MessagesRecyclerView) findViewById(R.id.messages_list))
-                .init(getLayerClient(), getPicasso())
+                .init(getLayerClient())
                 .addCellFactories(
                         new TextCellFactory(),
                         new ThreePartImageCellFactory(this, getLayerClient(), getPicasso()),
